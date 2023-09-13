a0720b1e9aa12152d6931a896da8c0c8340829e1
carloeusebi <carloeusebi@gmail.com>
2023-09-13 15:27:06 +0200

    Created mail controller

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class MailController extends Controller
+{
+    public function contactForm(Request $request)
+    {
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'email' => 'required|email',
+            'subject' => 'required',
+            'content' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        Mail::to(env('MAIL_TO_ADDRESS'))->send(new ContactMail($data));
+        return response()->json(null, 204);
+    }
+}
