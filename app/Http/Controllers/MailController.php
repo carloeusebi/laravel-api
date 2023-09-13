c70edb9b8516ea1816f5bc7762b8322a71806e87
carloeusebi <carloeusebi@gmail.com>
2023-09-13 17:59:55 +0200

    Saved contacts in the database

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ContactMail;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
@@ -17,11 +18,17 @@ class MailController extends Controller
         $validator = Validator::make($data, [
             'email' => 'required|email',
             'subject' => 'required',
-            'content' => 'required'
+            'content' => 'required',
+            'subscription' => 'nullable|boolean'
         ]);
 
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        if (isset($data['subscribe']) && $data['subscribe']) {
+            $already_subscribed = Contact::where('email', $data['email'])->count();
+            if (!$already_subscribed) Contact::create(['email' => $data['email']]);
         }
 
         Mail::to(env('MAIL_TO_ADDRESS'))->send(new ContactMail($data));
